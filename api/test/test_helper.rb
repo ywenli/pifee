5fc01e0a41181712964ffedc0714abb3729a935f
ywenli <kuota0204@gmail.com>
2021-02-22 16:35:55 +0900

    feat: Add seeds.rb to test_helper.rb #34

--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -3,11 +3,17 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
+  # number_of_processors: 使用しているマシン(Docker)のコア数が入る
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
