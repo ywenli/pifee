06834bc17e58ee10060c8db386c07721da8de5b2
ywenli <kuota0204@gmail.com>
2021-02-22 18:16:16 +0900

    feat: Add Tokenizable #32

---
+++ api/app/services/user_auth/tokenizable.rb
@@ -0,0 +1,25 @@
+module UserAuth
+  module Tokeni
+    def self.included(base)
+      # Userクラスで Tokenizable を include したときに 'base.extend ClassMethods' が実行される
+      base.extend ClassMethods
+    end
+
+    ## class method
+    module ClassMethods
+      # 渡されたトークンからユーザーを検索する
+      def from_token(token)
+        auth_token = AuthToken.new(token: token)
+        from_token_payload(auth_token.payload)
+      end
+
+      private
+
+        def from_token_payload(payload)
+          find(payload['sub'])
+        end
+    end
+
+    end
+  end
+end
