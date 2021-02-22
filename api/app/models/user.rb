ac5556f6ef368946eccb8a60930b91f04fb909e7
ywenli <kuota0204@gmail.com>
2021-02-22 15:37:55 +0900

    feat: Add validats for User model #34

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # gem bcrypt
+  has_secure_password
+
+  validates :name, presence: true,
+                    length: { maximum: 30, allow_blank: true}
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                        length: { minimum: 8 },
+                        format: {
+                          with: VALID_PASSWORD_REGEX
+                        },
+                        allow_nil: true,
+  validates :activated, inclusion: { in: [ true, false ]}
 end
