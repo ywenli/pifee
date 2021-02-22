7f9dc53f5a24be9d6e9455c409655e5d53a572f8
ywenli <kuota0204@gmail.com>
2021-02-22 10:51:10 +0900

    chore: Update rule of esling and prettier #27

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
   rules: {
     // 'generator-star-spacing': 'off',
     // 'no-debugger': process.env.BASE_URL === 'production' ? 'error' : 'off',
-    'prettier/prettier': 0,
+    'prettier/prettier': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // タグの最後を改行するか
@@ -32,18 +32,18 @@ module.exports = {
       multiline: 'always'
     }],
     // propsのtype宣言なし
-    // "vue/require-prop-types": 0,
+    // "vue/require-prop-types": 'off',
     // propsの初期値は設定しない
-    'vue-types/require-default-prop': 0,
+    'vue-types/require-default-prop': 'off',
     // propsのアッパーケース(myProp)、ケバブケース(my-prop)の使用を可とする
-    'vue/attribute-hyphenation': 0,
-    'vue/no-parsing-error': 0,
+    'vue/attribute-hyphenation': 'off',
+    'vue/no-parsing-error': 'off',
     // 不要なカッコは消す
-    'no-extra-parens': 1,
+    'no-extra-parens': 'warning',
     // 無駄なスペースは削除
-    'no-multi-spaces': 2,
+    'no-multi-spaces': 'error',
     // 使ってない変数があってもいい
-    '@typescript-eslint/no-unused-vars': 0,
-    'no-unused-vars': 0
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off'
   }
 }
