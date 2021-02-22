22fac0f46c1e58ecfc69ca0d1a838bd098e0de0f
ywenli <kuota0204@gmail.com>
2021-02-22 18:21:18 +0900

    docs: Add docs #32

--- api/app/services/user_auth/tokenizable.rb
+++ api/app/services/user_auth/tokenizable.rb
@@ -8,6 +8,8 @@ module UserAuth
     ## class method
     module ClassMethods
       # 渡されたトークンからユーザーを検索する
+      # @param [String] token トークン
+      # @return [String] トークンと合致するユーザー
       def from_token(token)
         auth_token = AuthToken.new(token: token)
         from_token_payload(auth_token.payload)
