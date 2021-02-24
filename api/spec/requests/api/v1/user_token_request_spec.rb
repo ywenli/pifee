ab071fdf7a17d2b675ecb640feb38419cadc8684
ywenli <kuota0204@gmail.com>
2021-02-24 18:45:18 +0900

    feat: Add Login controller #44

---
+++ api/spec/requests/api/v1/user_token_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::UserTokens", type: :request do
+
+  def user_token_logged_in(user)
+    params = { auth: { email: user.email, password: "password" } }
+    post api_url("/user_token"), params: params
+  end
+  def setup
+    @user = active_user
+    @key = UserAuth.token_access_key
+    user_token_logged_in(@user)
+  end
+
+  xdescribe '#create' do
+    it 'アクセストークンはCookieに保存されているか' do
+      cookie_token = @request.cookie_jar[@key]
+      expect(cookie_token).to be_present
+    end
+  end
+end
