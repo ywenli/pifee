7b102cbf2b2a620a95eb416bb76e960479083de5
ywenli <kuota0204@gmail.com>
2021-02-25 16:03:26 +0900

    feat: Add signup page #45

---
+++ front/components/atoms/BaseTextField.vue
@@ -0,0 +1,46 @@
+<template>
+  <ValidationProvider
+    v-slot='{ errors, valid }'
+    :name='$attrs.label'
+    :rules='rules'
+    :vid='$attrs.label'
+  >
+    <v-text-field
+      v-model='inputValue'
+      :error-messages='errors'
+      :success='valid'
+      v-bind='$attrs'
+      v-on='$listeners'
+    />
+  </ValidationProvider>
+</template>
+
+<script>
+export default {
+  props: {
+    rules: {
+      type: [Object, String],
+      default: ''
+    },
+    value: {
+      type: null,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    inputValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
