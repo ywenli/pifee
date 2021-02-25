1f167ea8935819d5ae9fbac780f60d8f48c68ba9
ywenli <kuota0204@gmail.com>
2021-02-25 17:16:41 +0900

    feat: Add signin page #48

---
+++ front/pages/login.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container fluid>
+    <v-row
+      align='center'
+      justify='center'
+    >
+      <v-col
+        cols='12'
+        class='my-8 text-center'
+      >
+        <h1 class='text-h5 font-weight-bold'>
+          ログイン
+        </h1>
+      </v-col>
+      <v-card
+        flat
+        width='80%'
+        max-width='320'
+        color='transparent'
+      >
+        <v-form>
+          <p v-if='error' class='errors'>
+            {{ error }}
+          </p>
+          <BaseTextField
+            v-model='email'
+            label='メールアドレス'
+            rules='max:255|required|email'
+            placeholder='your@email.com'
+            prepend-icon='mdi-email'
+          />
+          <BaseTextField
+            v-model='password'
+            label='パスワード'
+            rules='min:8|required'
+            placeholder='8文字以上'
+            prepend-icon='mdi-lock'
+            :type='show ? "text" : "password"'
+            :append-icon='show ? "mdi-eye" : "mdi-eye-off"'
+            autocomplete='on'
+            vid='password'
+            @click:append='show = !show'
+          />
+        </v-form>
+        <v-card-text class="px-0">
+          <v-row justify='center'>
+            <BaseButton
+              color='primary'
+              :loading='loading'
+              @click="login"
+            >
+              ログイン
+            </BaseButton>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import BaseTextField from '../components/atoms/BaseTextField.vue'
+import BaseButton from '../components/atoms/BaseButton.vue'
+export default {
+  components: {
+    BaseTextField,
+    BaseButton
+  },
+  data () {
+    return {
+      params: { auth: { email: '', password: '' } },
+      error: '',
+      show: 'false',
+      loading: false
+    }
+  },
+  methods: {
+    login () {
+      this.loading = true
+      setTimeout(() => {
+        this.$store.dispatch('login')
+        this.$router.replace('/')
+        this.loading = false
+      }, 1500)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
