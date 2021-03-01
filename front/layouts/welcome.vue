f80957300eb83bda5e6cff8cda7e1cda506114d2
ywenli <kuota0204@gmail.com>
2021-03-01 18:20:39 +0900

    feat: Add layout file before and after login #57

---
+++ front/layouts/welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <TheHeaderBeforeLogin />
+    <v-main>
+      <v-container>
+        <WelcomeAbout />
+        <WelcomePopular />
+        <WelcomeCategory />
+        <WelcomeJoin />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import TheHeaderBeforeLogin from '@/components/organisms/TheHeaderBeforeLogin.vue'
+import WelcomeAbout from '@/components/organisms/WelcomeAbout.vue'
+import WelcomePopular from '@/components/organisms/WelcomePopular.vue'
+import WelcomeCategory from '@/components/organisms/WelcomeCategory.vue'
+import WelcomeJoin from '@/components/organisms/WelcomeJoin.vue'
+export default {
+  components: {
+    TheHeaderBeforeLogin,
+    WelcomeAbout,
+    WelcomePopular,
+    WelcomeCategory,
+    WelcomeJoin
+  }
+}
+</script>
+
+<style>
+
+</style>
