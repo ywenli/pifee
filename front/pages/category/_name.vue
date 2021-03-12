70a9f2bc0061bd780583244372e5b42027767e5f
ywenli <kuota0204@gmail.com>
2021-03-12 17:35:30 +0900

    feat: Add works tagged in category page #73 :+1:
    タグ付けされた作品をカテゴリーページに表示した

--- front/pages/category/_name.vue
+++ front/pages/category/_name.vue
@@ -1,14 +1,53 @@
 <template>
-  <v-conteiner>
+  <v-container>
     <v-row>
-      カテゴリー詳細ページ
+      <v-col>
+        <!-- TODO: このタイトル部分のデザインを変更する -->
+        <h1>
+          {{ $route.params.name }}
+        </h1>
+      </v-col>
     </v-row>
-  </v-conteiner>
+    <v-row>
+      <v-col
+        v-for="work in works"
+        :key="work.id"
+        cols="12"
+        sm="6"
+        lg="4"
+      >
+        <BaseCardForWork
+          :to='`/account/works/${work.id}`'
+        >
+          <template #title>
+            {{ work.title }}
+          </template>
+          <template #user_id>
+            {{ work.user_id }}
+          </template>
+        </BaseCardForWork>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import BaseCardForWork from '@/components/organisms/BaseCardForWork.vue'
 export default {
-
+  components: { BaseCardForWork },
+  data () {
+    return {
+      works: [],
+      title: []
+    }
+  },
+  mounted () {
+    this.$axios.get(`/api/v1/tags/${this.$route.params.name}`)
+      .then((res) => {
+        this.works = res.data
+        this.title = [...res.data].map(value => value.title)
+      })
+  }
 }
 </script>
 
