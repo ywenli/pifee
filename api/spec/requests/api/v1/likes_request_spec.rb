1e9d1eaafb117ee5626218dc687e39917be17b50
ywenli <kuota0204@gmail.com>
2021-03-14 23:57:57 +0900

    feat: Add Likes controller #72
    Likesコントローラーを追加(index, create, destroy)

---
+++ api/spec/requests/api/v1/likes_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Likes", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/api/v1/likes/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/api/v1/likes/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/api/v1/likes/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/api/v1/likes/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
