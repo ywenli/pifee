9c2627e677d2423451c30e398e55d740563f60e6
ywenli <kuota0204@gmail.com>
2021-03-16 11:49:38 +0900

    feat: Add ranking method for today, weekly, all :+1:
    ランキングに関するメソッドを追加: today, weekly, all
    それぞれの期間のうち、いいね数の多い上位6つを取得する

--- api/app/controllers/api/v1/likes_controller.rb
+++ api/app/controllers/api/v1/likes_controller.rb
@@ -21,6 +21,25 @@ class Api::V1::LikesController < ApplicationController
     render json: @likes
   end
 
+  # 今日のランキングから上位6つを取得
+  # TODO: 以下3つのランキングメソッドは冗長なのでリファクタリングする
+  def get_top_6_in_today_ranking
+    @today_ranks = Work.find(Like.where(created_at: Time.current.all_day).group(:work_id).order('count(work_id) desc').limit(6).pluck(:work_id))
+    render json: @today_ranks
+  end
+
+  # 週間のランキングから上位6つを取得
+  def get_top_6_in_weekly_ranking
+    @weekly_ranks = Work.find(Like.where(created_at: Time.current.all_week).group(:work_id).order('count(work_id) desc').limit(6).pluck(:work_id))
+    render json: @weekly_ranks
+  end
+
+  # 全期間のランキングから上位6つを取得
+  def get_top_6_in_all_ranking
+    @all_ranks = Work.find(Like.group(:work_id).order('count(work_id) desc').limit(6).pluck(:work_id))
+    render json: @all_ranks
+  end
+
   private
 
     def likes_params
