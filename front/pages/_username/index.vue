4a4a054f4853eb279e148f2020739ec8b468878d
ywenli <kuota0204@gmail.com>
2021-03-17 16:30:34 +0900

    feat: Show user's own works in profile :+1:
    プロフィールページにユーザーの作品を表示

--- front/pages/_username/index.vue
+++ front/pages/_username/index.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>
-    ユーザ名: {{ $route.params.username }}
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        アイコン
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div class="text-h5">
+          {{ $route.params.username }}
+        </div>
+      </v-col>
+    </v-row>
+    <v-row class="mx-4">
+      <v-col
+        v-for="work in works"
+        :key="work.id"
+        cols="12"
+        sm="4"
+        lg="3"
+      >
+        <BaseCardForWork
+          :to="`/${work.user_id}/works/${work.id}`"
+        >
+          <template #title>
+            {{ work.title }}
+          </template>
+          <template #user_id>
+            {{ work.user_id }}
+          </template>
+        </BaseCardForWork>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import BaseCardForWork from '../../components/organisms/BaseCardForWork.vue'
 export default {
-
+  components: { BaseCardForWork },
+  data () {
+    return {
+      works: []
+    }
+  },
+  mounted () {
+    this.$axios.get('/api/v1/works', {
+      params: {
+        user_id: this.$auth.user.id
+      }
+    })
+      .then((res) => { this.works = res.data })
+  }
 }
 </script>
 
