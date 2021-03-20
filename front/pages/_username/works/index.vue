c36f2f2d00fbdf2dd6b542beb097a3155d10abfd
ywenli <kuota0204@gmail.com>
2021-03-20 17:55:44 +0900

    feat: Add public and private tabs #89 :sparkles:
    公開中タブと下書きタブを追加

--- front/pages/_username/works/index.vue
+++ front/pages/_username/works/index.vue
@@ -7,6 +7,7 @@
         </div>
       </v-col>
     </v-row>
+    {{ workPublic }}
     <v-row>
       <v-col>
         <v-card
@@ -23,17 +24,20 @@
               </div>
             </v-tab>
             <v-tab>
-              <div class="text-none">
+              <div class="text-none" @click="filterWorkPublic">
                 公開中
               </div>
             </v-tab>
             <v-tab>
-              <div class="text-none">
+              <div class="text-none" @click="filterWorkPrivate">
                 下書き
               </div>
             </v-tab>
 
-            <v-tab-item class="my-4">
+            <v-tab-item
+
+              class="my-4"
+            >
               <v-row class="mx-4">
                 <v-col
                   v-for="work in works"
@@ -46,6 +50,45 @@
                   >
                     <template #title>
                       {{ work.title }}
+                    </template>
+                  </BaseCardForDashboard>
+                </v-col>
+              </v-row>
+            </v-tab-item>
+            <v-tab-item class="my-4">
+              <v-row class="mx-4">
+                <v-col
+                  v-for="workPublic in worksPublic"
+                  :key="workPublic.id"
+                  cols="12"
+                >
+                  <BaseCardForDashboard
+                    :to="`/${$route.params.username}/works/${workPublic.id}/edit`"
+                    :isPublic="workPublic.is_public"
+                  >
+                    <template #title>
+                      {{ workPublic.title }}
+                    </template>
+                  </BaseCardForDashboard>
+                </v-col>
+              </v-row>
+            </v-tab-item>
+            <v-tab-item
+
+              class="my-4"
+            >
+              <v-row class="mx-4">
+                <v-col
+                  v-for="workPrivate in worksPrivate"
+                  :key="workPrivate.id"
+                  cols="12"
+                >
+                  <BaseCardForDashboard
+                    :to="`/${$route.params.username}/works/${workPrivate.id}/edit`"
+                    :isPublic="workPrivate.is_public"
+                  >
+                    <template #title>
+                      {{ workPrivate.title }}
                     </template>
                   </BaseCardForDashboard>
                 </v-col>
@@ -67,7 +110,9 @@ export default {
   data () {
     return {
       tab: null,
-      works: []
+      works: [],
+      worksPublic: [],
+      worksPrivate: []
     }
   },
   mounted () {
@@ -75,6 +120,14 @@ export default {
       .then((res) => {
         this.works = res.data
       })
+  },
+  methods: {
+    filterWorkPublic () {
+      this.worksPublic = this.works.filter(work => work.is_public === true)
+    },
+    filterWorkPrivate () {
+      this.worksPrivate = this.works.filter(work => work.is_public === false)
+    }
   }
 }
 </script>
