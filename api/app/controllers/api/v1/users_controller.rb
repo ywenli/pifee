47fd7be3ffaebc200dd55628f717f36470c5d76c
ywenli <kuota0204@gmail.com>
2021-03-23 18:41:43 +0900

    fix: Fixed problem with profile not being changed #93
    プロフィールが変更されない問題を修正

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_user
+  #before_action :authenticate_user
   def index
     @users = User.all
     render json: @users
