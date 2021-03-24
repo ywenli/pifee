d61d694c658b1b2643720d1d3cccbead489d5482
ywenli <kuota0204@gmail.com>
2021-03-24 15:25:31 +0900

    feat: Use TheCardsForCategory component in top and search
    トップページと検索ページでTheCardsForCategoryコンポーネントを使う

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -19,48 +19,20 @@
         </div>
       </v-col>
     </v-row>
-    <v-row>
-      <!-- TODO: organismsコンポーネントにまとめる -->
-      <v-col
-        v-for="tag in tags"
-        :key="tag"
-        cols="4"
-        sm="3"
-        lg="2"
-      >
-        <BaseCardForCategory
-          v-model="tags"
-          :to='`/category/${tag}`'
-        >
-          {{ tag }}
-        </BaseCardForCategory>
-      </v-col>
-    </v-row>
+    <TheCardsForCategory />
   </v-container>
 </template>
 
 <script>
 import TheTabsForRanking from '@/components/organisms/TheTabsForRanking.vue'
-import BaseCardForCategory from '@/components/organisms/BaseCardForCategory.vue'
-
+import TheCardsForCategory from '@/components/organisms/TheCardsForCategory.vue'
 export default {
   components: {
     TheTabsForRanking,
-    BaseCardForCategory
+    TheCardsForCategory
   },
   layout ({ $auth }) {
     return $auth.loggedIn ? 'default' : 'welcome'
-  },
-  data () {
-    return {
-      tags: []
-    }
-  },
-  mounted () {
-    this.$axios.get('/api/v1/tags')
-      .then((res) => {
-        this.tags = [...res.data].map(value => value.name)
-      })
   }
 }
 </script>
