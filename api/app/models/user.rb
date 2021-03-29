9523e3e8714167639e4faafbea4daaadd23ef14b
ywenli <63993873+ywenli@users.noreply.github.com>
2021-03-29 18:17:55 +0900

    test: Add model test #108 :green_heart: (#109)

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,7 +1,10 @@
+require 'validator/email_validator'
+
 class User < ApplicationRecord
   has_many :works, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_works, through: :likes, source: :work
+  before_validation :downcase_email
   include UserAuth::Tokenizable
   # gem bcrypt
   has_secure_password
@@ -11,13 +14,16 @@ class User < ApplicationRecord
                   length: { maximum: 30, allow_blank: true}
   validates :name, presence: true,
                     length: { maximum: 30, allow_blank: true}
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: {case_sensitive: false},
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
                         length: { minimum: 8 },
                         format: {
-                          with: VALID_PASSWORD_REGEX,
-                          message: :invalid_password
+                          with: VALID_PASSWORD_REGEX
                         },
                         allow_nil: true
 
@@ -41,4 +47,11 @@ class User < ApplicationRecord
   def my_json
     as_json(only: [:id, :url, :name, :email, :created_at])
   end
+
+  private
+
+    def downcase_email
+      self.email.downcase! if email
+    end
+
 end
--- api/spec/models/like_spec.rb
+++ api/spec/models/like_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:work) { create(:public) }
+  it 'いいねをしたことがないworkに新規でいいねをする場合、有効である' do
+    like = create(:like, user_id: user.id, work_id: work.id)
+    expect(like).to be_valid
+  end
+  it 'いいねをしたことがあるworkに重複していいねする場合、無効である' do
+    like = create(:like, user_id: user.id, work_id: work.id)
+    like = build(:like, user_id: user.id, work_id: work.id)
+    like.valid?
+    expect(like.errors.full_messages).to include('Workはすでに存在します')
+  end
 end
