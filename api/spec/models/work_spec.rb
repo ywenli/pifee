fadf0935a3dc1f83ed1d53a27b0de2d8161bea4c
ywenli <kuota0204@gmail.com>
2021-03-29 17:25:35 +0900

    test: Add work model test #108
    Workモデルテストを追加

--- api/spec/models/work_spec.rb
+++ api/spec/models/work_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'タイトルが存在する場合、有効である' do
+    work = create(:public, title: 'タイトル')
+    expect(work).to be_valid
+  end
+  it 'タイトルが存在しない場合、無効である' do
+    work = build(:public, title: nil)
+    work.valid?
+    expect(work.errors.full_messages).to include('タイトルを入力してください')
+  end
 end
