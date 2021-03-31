d04ed9454908345aa42ea196037374da633a46b4
ywenli <kuota0204@gmail.com>
2021-03-31 18:28:00 +0900

    refactor: Format code #115
    コードをフォーマット

--- api/db/seeds/development/likes.rb
+++ api/db/seeds/development/likes.rb
@@ -1,27 +1,27 @@
 # 全てのランキング用のseedデータ
 # いいね数が段階的に1ずつ増えているlikeデータを作成
-1.times{ create(:like, :for_seeds_user_1) }
-2.times{ create(:like, :for_seeds_user_2) }
-3.times{ create(:like, :for_seeds_user_3) }
-4.times{ create(:like, :for_seeds_user_4) }
-5.times{ create(:like, :for_seeds_user_5) }
-6.times{ create(:like, :for_seeds_user_6) }
-7.times{ create(:like, :for_seeds_user_7) }
+1.times{ create(:like, :for_seeds_user1) }
+2.times{ create(:like, :for_seeds_user2) }
+3.times{ create(:like, :for_seeds_user3) }
+4.times{ create(:like, :for_seeds_user4) }
+5.times{ create(:like, :for_seeds_user5) }
+6.times{ create(:like, :for_seeds_user6) }
+7.times{ create(:like, :for_seeds_user7) }
 
 # 週間のランキング用のseedデータ
-1.times{ create(:like, :with_weekly, :for_seeds_user_11) }
-2.times{ create(:like, :with_weekly, :for_seeds_user_12) }
-3.times{ create(:like, :with_weekly, :for_seeds_user_13) }
-4.times{ create(:like, :with_weekly, :for_seeds_user_14) }
-5.times{ create(:like, :with_weekly, :for_seeds_user_15) }
-6.times{ create(:like, :with_weekly, :for_seeds_user_16) }
-7.times{ create(:like, :with_weekly, :for_seeds_user_17) }
+1.times{ create(:like, :with_weekly, :for_seeds_user11) }
+2.times{ create(:like, :with_weekly, :for_seeds_user12) }
+3.times{ create(:like, :with_weekly, :for_seeds_user13) }
+4.times{ create(:like, :with_weekly, :for_seeds_user14) }
+5.times{ create(:like, :with_weekly, :for_seeds_user15) }
+6.times{ create(:like, :with_weekly, :for_seeds_user16) }
+7.times{ create(:like, :with_weekly, :for_seeds_user17) }
 
 # デイリーランキング用のseedデータ
-1.times{ create(:like, :with_daily, :for_seeds_user_21) }
-2.times{ create(:like, :with_daily, :for_seeds_user_22) }
-3.times{ create(:like, :with_daily, :for_seeds_user_23) }
-4.times{ create(:like, :with_daily, :for_seeds_user_24) }
-5.times{ create(:like, :with_daily, :for_seeds_user_25) }
-6.times{ create(:like, :with_daily, :for_seeds_user_26) }
-7.times{ create(:like, :with_daily, :for_seeds_user_27) }
+1.times{ create(:like, :with_daily, :for_seeds_user21) }
+2.times{ create(:like, :with_daily, :for_seeds_user22) }
+3.times{ create(:like, :with_daily, :for_seeds_user23) }
+4.times{ create(:like, :with_daily, :for_seeds_user24) }
+5.times{ create(:like, :with_daily, :for_seeds_user25) }
+6.times{ create(:like, :with_daily, :for_seeds_user26) }
+7.times{ create(:like, :with_daily, :for_seeds_user27) }
--- api/spec/factories/likes.rb
+++ api/spec/factories/likes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :like do
     sequence(:user_id) { |n| n }
@@ -15,91 +17,91 @@ FactoryBot.define do
     end
 
     # 全てのランキングで表示させるseedデータ用のtrait(いいね数が段階的にに1ずつ増えいているlikeデータ)
-    trait :for_seeds_user_1 do
+    trait :for_seeds_user1 do
       user_id { 1 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_2 do
+    trait :for_seeds_user2 do
       user_id { 2 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_3 do
+    trait :for_seeds_user3 do
       user_id { 3 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_4 do
+    trait :for_seeds_user4 do
       user_id { 4 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_5 do
+    trait :for_seeds_user5 do
       user_id { 5 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_6 do
+    trait :for_seeds_user6 do
       user_id { 6 }
       sequence(:work_id) { |n| n }
     end
-    trait :for_seeds_user_7 do
+    trait :for_seeds_user7 do
       user_id { 7 }
       sequence(:work_id) { |n| n }
     end
 
     # 週間のランキングで表示させるseedデータ用のtrait(いいね数が段階的にに1ずつ増えいているlikeデータ)
-    trait :for_seeds_user_11 do
+    trait :for_seeds_user11 do
       user_id { 11 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_12 do
+    trait :for_seeds_user12 do
       user_id { 12 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_13 do
+    trait :for_seeds_user13 do
       user_id { 13 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_14 do
+    trait :for_seeds_user14 do
       user_id { 14 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_15 do
+    trait :for_seeds_user15 do
       user_id { 15 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_16 do
+    trait :for_seeds_user16 do
       user_id { 16 }
       sequence(:work_id) { |n| n + 10 }
     end
-    trait :for_seeds_user_17 do
+    trait :for_seeds_user17 do
       user_id { 17 }
       sequence(:work_id) { |n| n + 10 }
     end
 
     # デイリーランキングで表示させるseedデータ用のtrait(いいね数が段階的にに1ずつ増えいているlikeデータ)
-    trait :for_seeds_user_21 do
+    trait :for_seeds_user21 do
       user_id { 21 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_22 do
+    trait :for_seeds_user22 do
       user_id { 22 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_23 do
+    trait :for_seeds_user23 do
       user_id { 23 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_24 do
+    trait :for_seeds_user24 do
       user_id { 24 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_25 do
+    trait :for_seeds_user25 do
       user_id { 25 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_26 do
+    trait :for_seeds_user26 do
       user_id { 26 }
       sequence(:work_id) { |n| n + 20 }
     end
-    trait :for_seeds_user_27 do
+    trait :for_seeds_user27 do
       user_id { 27 }
       sequence(:work_id) { |n| n + 20 }
     end
