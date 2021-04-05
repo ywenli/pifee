9953229589ce6423ec75d7515929075da7c8e833
ywenli <kuota0204@gmail.com>
2021-04-05 23:44:14 +0900

    test: Add unit test #117
    ユニットテストを追加

--- front/setup.js
+++ front/setup.js
@@ -4,3 +4,7 @@ import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 Vue.config.devtools = false
 Vue.config.productionTip = false
+
+const app = document.createElement('div')
+app.setAttribute('data-app', true)
+document.body.appendChild(app)
